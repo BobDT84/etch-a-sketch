9ee32505fc65da32b33ed9139399e7f133406671
Bob <bobdt84@gmail.com>
2022-03-08 18:49:47 -0600

    Changed #grid div to have a height of 100vh so it doesn't go out of the screen.  Added a clear button, and currently working on the clear function to toggle the mouseOver class off

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function makeGridOld(size = 16){
-    const grid = document.querySelector('#grid');
-    let row,column;
-    for(let i = 1; i <=size; i++){ //If size = 16 then create a 16x16 grid of divs
-        row = document.createElement('div');
-        row.className = "row";
-        grid.append(row);
-
-        for(let i = 1; i <=size; i++){
-            column = document.createElement('div');
-            column.className = "column";
-            column.addEventListener('mouseover', changeColor);
-            row.append(column);
-        }
-    }
-}
-
 function makeGrid(size=16){
     const grid = document.querySelector('#grid');
     grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
@@ -23,6 +6,7 @@ function makeGrid(size=16){
     for(let i = 0; i<size**2; i++){
         block = document.createElement('div');
         block.className = 'block';
+        block.addEventListener('mouseover', changeColor);
         grid.append(block);
     }
 }
@@ -31,4 +15,7 @@ function changeColor(event){
     event.target.classList.add('mouseOver');
 }
 
+function clear(){
+    let block = document.getElementsByClassName('mouseOver');
+}
 makeGrid(30);
